5311e8eb68e75e010595f84af481a27156851c5d
Rob Dobson <robdobsn@users.noreply.github.com>
2024-01-18 21:39:07 +0000

    Moved WebServer SysMod into this library

---
+++ components/WebServer/RegisterWebServerSysMod.h
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Register WebServer SysMods
+// Register the WebServer SysMod with the SysManager
+//
+// Rob Dobson 2018-2024
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "sdkconfig.h"
+#include "WebServer.h"
+
+// Check if networking is enabled
+#if defined(CONFIG_ESP_WIFI_ENABLED) || defined(CONFIG_ETH_USE_ESP32_EMAC) || defined(CONFIG_ETH_USE_SPI_ETHERNET) || defined(CONFIG_ETH_USE_OPENETH) || defined(CONFIG_ETH_USE_RMII_ETHERNET)
+#define NETWORKING_IS_ENABLED
+#endif
+
+namespace RegisterSysMods
+{
+    void registerSysMods(SysManager& sysManager)
+    {
+        // WebServer
+#ifdef NETWORKING_IS_ENABLED
+        sysManager.registerSysMod("WebServer", WebServer::create, false, "NetMan");
+#endif
+    }
+}
+
